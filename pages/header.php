b5ccfa51718c0bdbd2bbcb4419e0b36381e81f87
milicaamitrovic <milicamitrovicfon@gmail.com>
2023-01-27 13:24:59 +0100

    Kreiran i stilizovan header

---
+++ pages/header.php
@@ -0,0 +1,19 @@
+<div class='navbar_div'>
+  <nav class="navbar navbar-light sticky-top bottom_sep" style="background-color: #f0e68c;">
+    <img src="../img/logo.png" class="logo-pic"/>
+    <a class="navbar-brand a_header" href="#"><p class="text-white">Meni</p></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Vrste plesova</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="termini.php">Termini</a>
+        </li>
+        </ul>
+    </div>
+  </nav>
+</div>
